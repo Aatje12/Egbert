318fb87eca93b53433a3556c964dfcb1e233c96f
Aatje12 <78856420+Aatje12@users.noreply.github.com>
2021-12-13 14:18:14 +0100

    Added crosshair ingame

--- EgbertSpel/Assets/PlayerMovement.cs
+++ EgbertSpel/Assets/PlayerMovement.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-
     public Rigidbody2D rb;
+    public Animator animator;
+    public GameObject crosshair;
 
     Vector2 movement;
+    Vector2 mousePos;
 
 
 
@@ -17,11 +19,24 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        Aim();
+
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void Aim() {
+        if (movement != Vector2.zero) {
+            crosshair.transform.localPosition = movement;
+        }
     }
 
 }
